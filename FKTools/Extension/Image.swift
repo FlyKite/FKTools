613913f3a8304ce71a00b8b4c8cf90c5ff29cabe
FlyKite <DogeFlyKite@gmail.com>
2017-10-11 20:25:11 +0800

    Image resizing and cropping

--- FKTools/Extension/Image.swift
+++ FKTools/Extension/Image.swift
@@ -20,6 +20,44 @@ extension UIImage {
         }
     }
     
+    func resizeWidth(to width: CGFloat) -> UIImage? {
+        let height = self.size.height * width / self.size.width
+        return self.resize(to: CGSize(width: width, height: height))
+    }
+    
+    func resizeHeight(to height: CGFloat) -> UIImage? {
+        let width = self.size.width * height / self.size.height
+        return self.resize(to: CGSize(width: width, height: height))
+    }
+    
+    func resize(to maxWidthOrHeight: CGFloat) -> UIImage? {
+        if maxWidthOrHeight < self.size.width && maxWidthOrHeight < self.size.height {
+            return self
+        } else if self.size.width > self.size.height {
+            return self.resizeWidth(to: maxWidthOrHeight)
+        } else if self.size.width < self.size.height {
+            return self.resizeHeight(to: maxWidthOrHeight)
+        } else {
+            return self.resize(to: CGSize(width: maxWidthOrHeight, height: maxWidthOrHeight))
+        }
+    }
+    
+    func resize(to size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, self.opaque, self.scale)
+        self.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    func cropping(in rect: CGRect) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(rect.size, self.opaque, self.scale)
+        self.draw(in: CGRect(x: -rect.origin.x, y: -rect.origin.y, width: self.size.width, height: self.size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
     func jpegData(with compressionQuality: CGFloat) -> Data? {
         return UIImageJPEGRepresentation(self, compressionQuality)
     }
