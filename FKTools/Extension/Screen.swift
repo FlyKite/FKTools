3a9a82f7e4ca0d739cbf72a3a4d2c562f36ea82f
FlyKite <DogeFlyKite@gmail.com>
2017-10-12 00:38:07 +0800

    Add Screen.swift

---
+++ FKTools/Extension/Screen.swift
@@ -0,0 +1,20 @@
+//
+//  Screen.swift
+//  FKToolsDemo
+//
+//  Created by FlyKite on 2017/10/12.
+//  Copyright © 2017年 Doge Studio. All rights reserved.
+//
+
+import UIKit
+
+class Screen: NSObject {
+    
+    static let width: CGFloat = UIScreen.main.bounds.width
+    static let height: CGFloat = UIScreen.main.bounds.height
+    static let scale: CGFloat = UIScreen.main.scale
+    
+    static func takeScreenshot(withStatusBar: Bool = true) {
+        
+    }
+}
