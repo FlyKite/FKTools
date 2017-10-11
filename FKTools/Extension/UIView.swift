df99625e02a116c7bd33fbbca46a23979b66c5d0
FlyKite <DogeFlyKite@gmail.com>
2017-10-12 00:26:21 +0800

    Add extension of UIView, takeSnapshot

---
+++ FKTools/Extension/UIView.swift
@@ -0,0 +1,79 @@
+//
+//  UIView.swift
+//  FKToolsDemo
+//
+//  Created by FlyKite on 2017/10/12.
+//  Copyright © 2017年 Doge Studio. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    var x: CGFloat {
+        get {
+            return self.frame.origin.x
+        }
+        set {
+            var frame = self.frame
+            frame.origin.x = newValue
+            self.frame = frame
+        }
+    }
+    
+    var y: CGFloat {
+        get {
+            return self.frame.origin.y
+        }
+        set {
+            var frame = self.frame
+            frame.origin.y = newValue
+            self.frame = frame
+        }
+    }
+    
+    var width: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set {
+            var frame = self.frame
+            frame.size.width = newValue
+            self.frame = frame
+        }
+    }
+    
+    var height: CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        set {
+            var frame = self.frame
+            frame.size.height = newValue
+            self.frame = frame
+        }
+    }
+    
+    var right: CGFloat {
+        get {
+            return self.frame.origin.x + self.width
+        }
+    }
+    
+    var bottom: CGFloat {
+        get {
+            return self.frame.origin.y + self.height
+        }
+    }
+
+    func takeSnapshot() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(UIScreen.main.bounds.size, true, UIScreen.main.scale)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        self.layer.render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
